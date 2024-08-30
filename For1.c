f8060ca5dfd76abfe62a795221231083f02293e2
Bhushan-code <nagpureb7@gmail.com>
2024-08-30 21:13:45 +0530

    30/08/24 Iteration

---
+++ For1.c
@@ -0,0 +1,15 @@
+//print jai ganesh five times
+
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= 5; iCnt++)
+    {
+        printf("Jai Ganesh... \n");
+    }
+
+    return 0;
+}
