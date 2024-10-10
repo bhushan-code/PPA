778c6a5c26c75b6c40b01dffdcf81acb72fa5692
Bhushan-Code <nagpureb7@gmail.com>
2024-10-10 19:22:09 +0530

    10/10/24 final in java

---
+++ FinalDemo1.java
@@ -0,0 +1,22 @@
+class FinalDemo1
+{
+    public static void main(String Arg[])
+    {
+        public int No1;
+
+        public void Fun()
+        {
+            System.out.println("Inside the Fun");
+        }
+    }
+}
+
+class Derived extends FinalDemo1
+{
+    public int No3;
+
+    public void Gun()
+    {
+        System.out.println("Inside the Gun");
+    }
+}
