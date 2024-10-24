1e9bcfe64a23535cf3f08c2532c4433a8951b8d0
bhushan-code <nagpureb7@gmail.com>
2024-10-24 18:05:25 +0530

    24/10/24

---
+++ StringDemo.java
@@ -0,0 +1,19 @@
+
+class StringDemo
+{
+    public static void main(String Arg[])
+    {
+        String s1 = "Marvellous";
+        String s2 = new String("PPA");
+        String s3 = "Python";
+        String s4 = "Marvellous";
+        String s5 = new String("PPA");
+        String s6 = "Java";
+        String s7 = new String("Pune");
+
+        System.out.println("Length of string is : "+s1.length());       
+    }
+}
+                                     
+
+ //lenght is method here in string check case (method always lower case)                                    
---
+++ StringDemo2.java
@@ -0,0 +1,13 @@
+
+class StringDemo
+{
+    public static void main(String Arg[])
+    {
+        StringBuffer s1 = new StringBuffer("Marvellous");
+        StringBuilder s2 = new StringBuilder("Marvellous");
+
+        s1.append("Infosystems");                                               //joining 2 string
+
+        System.out.println(s1);            
+    }
+}
